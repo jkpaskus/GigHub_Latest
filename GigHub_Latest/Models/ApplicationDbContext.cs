d7548d8e41189829e5db2a6d48fc7b498451e2c6
Jonas <jkpaskus@gmail.com>
2017-09-01 21:46:47 -0400

    Updated Database

---
+++ GigHub_Latest/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+namespace GigHub_Latest.Models
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
